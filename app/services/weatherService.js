2aff22b98899e869d7230c72905f8318730fb9c9
gitikx <gitikx@gmail.com>
2019-10-16 17:07:50 +0300

    updated variable's name

--- app/services/weatherService.js
+++ app/services/weatherService.js
@@ -2,15 +2,15 @@ app.service("weatherService", weatherService);
 
 function weatherService(apiService, $q) {
     this.updateWeather = function () {
-        let promises = [], dataTwo = [];
+        let promises = [], data = [];
         cities.forEach(function (city) {
             promises.push(apiService.get('weather', {q: city.requestParameter}));
         });
         $q.all(promises).then(function (results) {
             results.forEach(function (element) {
-                dataTwo.push(element.data);
+                data.push(element.data);
             })
         });
-        return dataTwo;
+        return data;
     };
 }
